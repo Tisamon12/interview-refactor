4615060aaa8fea66f8d26e515eb4e2a8e01925b1
Michal Krakowiak <michal.krakowiak@enginearch.com>
2018-05-29 23:09:30 +0200

    Added incrementing episode number

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,3 +1,9 @@
 class Episode < ActiveRecord::Base
   belongs_to :tv_show
+  before_create :set_episode_number
+
+  def set_episode_number
+    last_number = self.tv_show.episodes.last.try(:episode)
+    last_number == nil ? self.episode = 1 : self.episode = last_number + 1
+  end
 end
