b4437999b6ab1ff1822a4cf859c0dbbcdffe7cab
Michal Krakowiak <michal.krakowiak@enginearch.com>
2018-05-29 21:15:13 +0200

    Added column with api key to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150210105030) do
+ActiveRecord::Schema.define(version: 20180529191316) do
 
   create_table "episodes", force: :cascade do |t|
     t.integer  "tv_show_id", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20150210105030) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "api_key"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
